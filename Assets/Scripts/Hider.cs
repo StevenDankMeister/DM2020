ef3013df12ddfdaabd734c3e80f76da077d7be5e
thedankpure@gmail.com <thedankpure@gmail.com>
2020-02-29 00:13:03 +0100

    added lights and hiding area thingy

---
+++ Assets/Scripts/Hider.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hider : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] private GameObject lamps;
+    private Transform lamps_transform;
+    private GameObject light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        lamps_transform = lamps.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.tag == "Player"){
+            HideShowLamps(lamps, false);
+            spriteRenderer.enabled = true;
+        }    
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if(collision.gameObject.tag == "Player"){
+            HideShowLamps(lamps, true);
+            spriteRenderer.enabled = false;
+        }    
+    }
+
+    private void HideShowLamps(GameObject lamps, bool hide){
+        foreach(Transform lamp in lamps_transform){
+            lamp.Find("Light").gameObject.SetActive(hide);
+        }
+    }
+}
